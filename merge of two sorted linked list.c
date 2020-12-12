c3247496d826f83c2ab3722c5cf1b5dfb8c3799b
Tuhin Mukherjee <59216687+whitexgod@users.noreply.github.com>
2020-12-12 09:15:37 +0530

    merge of two sorted linked list

--- merge of two sorted linked list.c
+++ merge of two sorted linked list.c
@@ -6,115 +6,109 @@ struct node
 {
 	int data;
 	struct node *next;
-};
+} *temp;
 
-struct node *head1,*head2,*newnode,*tempnode1,*tempnode2;
-struct node *sorting_head,*sorting_tail;
+struct node *head1, *head2, *newnode, *tempnode1, *tempnode2;
+struct node *sorting_head, *sorting_tail;
 
-main()
+void main()
 {
-	int i,n1,n2;
+	int i, n1, n2;
 	printf("\nFor the first sorted linked list :- \n");
 	printf("Enter the number of nodes to be created : ");
-	scanf("%d",&n1);
-	head1=0;
-	for(i=0;i<n1;i++)
+	scanf("%d", &n1);
+	head1 = NULL;
+	for (i = 0; i < n1; i++)
 	{
-		newnode=(struct node*)malloc(sizeof(struct node));
-		printf("\nEnter the data for the %d node : ",i+1);
-		scanf("%d",&newnode->data);
-		newnode->next=0;
-		if(head1==0)
+		newnode = (struct node *)malloc(sizeof(struct node));
+		printf("\nEnter the data for the %d node : ", i + 1);
+		scanf("%d", &newnode->data);
+		newnode->next = NULL;
+		if (head1 == NULL)
 		{
-			head1=tempnode1=newnode;
+			head1 = tempnode1 = newnode;
 		}
 		else
 		{
-			tempnode1->next=newnode;
-			tempnode1=newnode;
+			tempnode1->next = newnode;
+			tempnode1 = newnode;
 		}
 	}
-	
+
 	printf("\nFor the second sorted linked list :- \n");
 	printf("Enter the number of nodes to be created : ");
-	scanf("%d",&n2);
-	head2=0;
-	for(i=0;i<n2;i++)
+	scanf("%d", &n2);
+	head2 = NULL;
+	for (i = 0; i < n2; i++)
 	{
-		newnode=(struct node*)malloc(sizeof(struct node));
-		printf("\nEnter the data for the %d node : ",i+1);
-		scanf("%d",&newnode->data);
-		newnode->next=0;
-		if(head2==0)
+		newnode = (struct node *)malloc(sizeof(struct node));
+		printf("\nEnter the data for the %d node : ", i + 1);
+		scanf("%d", &newnode->data);
+		newnode->next = NULL;
+		if (head2 == NULL)
 		{
-			head2=tempnode2=newnode;
+			head2 = tempnode2 = newnode;
 		}
 		else
 		{
-			tempnode2->next=newnode;
-			tempnode2=newnode;
+			tempnode2->next = newnode;
+			tempnode2 = newnode;
 		}
 	}
-	
-	sorting_head=sorting_tail=0;
-	if(head1==0)
+
+	sorting_head = sorting_tail = NULL;
+	if (head1 == NULL)
 	{
-		sorting_head=head2;
-		sorting_tail=tempnode2;
+		sorting_head = head2;
+		sorting_tail = tempnode2;
 	}
-	else if(head2==0)
+	else if (head2 == NULL)
 	{
-		sorting_head=head1;
-		sorting_tail=tempnode1;
+		sorting_head = head1;
+		sorting_tail = tempnode1;
 	}
 	else
 	{
-		do
+		if ((head1->data) < (head2->data))
+		{	
+			sorting_head = head1;
+			head1 = head1->next;
+		}
+		else
 		{
-			if((head1->data) < (head2->data))
+			sorting_head = head2;
+			head2 = head2->next;
+		}
+		sorting_tail = sorting_head;
+		while ((head1 != NULL) && (head2 != NULL))
+		{
+			if ((head1->data) < (head2->data))
 			{
-				if(sorting_head==0)
-				{
-					sorting_head=sorting_tail=head1;
-					head1=sorting_tail->next;
-				}
-				else
-				{
-					sorting_tail->next=head1;
-					sorting_tail=head1;
-					head1=sorting_tail->next;
-				}	
+				sorting_tail->next = head1;
+				sorting_tail = sorting_tail->next;
+				head1 = head1->next;
 			}
 			else
 			{
-				if(sorting_head==0)
-				{
-					sorting_head=sorting_tail=head2;
-					head2=sorting_tail->next;
-				}
-				else
-				{
-					sorting_tail->next=head2;
-					sorting_tail=head2;
-					head2=sorting_tail->next;
-				}
+				sorting_tail->next = head2;
+				sorting_tail = sorting_tail->next;
+				head2 = head2->next;
 			}
-		}while( ( head1 !=0 ) || ( head2 != 0 ) );
-		if(head1==0)
-		{
-			sorting_tail->next=head2;
 		}
-		if(head2==0)
+		if (head1 == NULL)
 		{
-			sorting_tail->next=head1;
-		}		
+			sorting_tail->next = head2;
+		}
+		if (head2 == NULL)
+		{
+			sorting_tail->next = head1;
+		}
 	}
 	printf("\nThe sorted linked list is as follows :- \n ");
-	struct node *temp;
-	temp=sorting_head;
-	while (temp !=0)
+	temp = sorting_head;
+	while (temp != NULL)
 	{
 		printf(" %d ", temp->data);
-		temp=temp->next;
+		temp = temp->next;
 	}
 }
